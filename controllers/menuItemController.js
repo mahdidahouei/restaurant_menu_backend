df926985064c1f60fed7fd679d13e64ca9ddcd33
MahdiArabpour <mahdi.a.200066@gmail.com>
2024-04-09 10:32:40 +0330

    changed image url in response

--- controllers/menuItemController.js
+++ controllers/menuItemController.js
@@ -100,7 +100,7 @@ exports.getMenuItems = async (req, res) => {
         menuItems: categoryMenuItems.map(item => ({
           _id: item._id,
           title: item.title,
-          imageUrl: item.imageUrl,
+          imageUrl: `${req.protocol}://${req.get('host')}/api/menuItems/${item._id}/image`,
           ingredients: item.ingredients,
           price: item.price
         }))
